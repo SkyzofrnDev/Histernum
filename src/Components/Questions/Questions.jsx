d17e5524a046e19fa114c438096fcc92ee1d3a96
SkyzofrnDev <dirgahardekaagustiantara@gmail.com>
2025-09-16 19:33:31 +0700

    question update

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -8,8 +8,8 @@ import { Link } from "react-router-dom";
 
 const Questions = () => {
   return (
-    <div className="w-full select-none h-screen flex flex-col bg-[#131f24]">
-      <div className="flex items-center  gap-4 text-[#ed4140] font-semibold just text-2xl">
+    <div className="w-full select-none h-screen flex flex-col bg-[#131f24] py-10 ">
+      <div className="flex items-center  gap-4 text-[#ed4140] font-semibold just text-2xl px-36 w-full justify-between py-5">
         <Link to={"/"}>
           <img
             src="https://d35aaqx5ub95lt.cloudfront.net/images/df223d5b9feb8017b323ed21103eb5ac.svg"
@@ -17,13 +17,15 @@ const Questions = () => {
             loading="lazy"
           />
         </Link>
-        <img
-          src="/Icons/heart.svg"
-          alt="icon-heart"
-          loading="lazy"
-          className="w-10"
-        />
-        <p className="mt-1 ">5</p>
+        <div className="flex items-center">
+          <img
+            src="/Icons/heart.svg"
+            alt="icon-heart"
+            loading="lazy"
+            className="w-10"
+          />
+          <p className="mt-1 ">5</p>
+        </div>
       </div>
       <div></div>
       <div className="border-t-2 border-[#37464f] py-10">
